aadb9003c5688672246ce91a8cbed35052d60691
CEO <aligithub501@gmail.com>
2025-02-22 14:39:03 -0500

    Add files via upload

---
+++ Win_File/Req.cmd
@@ -0,0 +1,7 @@
+@ECHO OFF
+
+START /B pip3 install -r requirements.txt  2>NUL >NUL
+echo INSTALLING REQUIREMENTS DO NOT CLOSE THIS WINDOW MANUALLY...
+cd ../
+echo path= %CD% >>CEO_OSINT/Configuration/Configuration.ini
+echo Desktop>CEO_OSINT/Display/Display.txt
