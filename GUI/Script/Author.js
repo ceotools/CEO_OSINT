174290230e94a062338f81f78f324c4a4b727e2d
CEO <aligithub501@gmail.com>
2025-02-23 13:54:44 -0500

    Add files via upload

---
+++ GUI/Script/Author.js
@@ -0,0 +1,11 @@
+function English(){
+    alert("This tool has been made by CEO (CEO) in a dark world Thank you for have downloaded my Tool :)");
+}
+
+function Italiano(){
+    alert("Questo tool è stato creato da CEO (CEO) in  per aver scaricato il mio tool :)");
+}
+
+function French(){
+    alert("Cette tool A été créé par CEO O)Sicilia a p beacoup pour avoir télécharge mon Tool :)");
+}
