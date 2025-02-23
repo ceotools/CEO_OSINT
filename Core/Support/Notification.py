874f0eb5be8dc557aa3eb0d625396fdc17b8d5c9
CEO <aligithub501@gmail.com>
2025-02-23 15:30:00 -0500

    Add files via upload

---
+++ Core/Support/Notification.py
@@ -0,0 +1,13 @@
+import os
+
+class Notifier:
+
+    @staticmethod
+    def Start(Mode):
+        if Mode == "Desktop":
+            if os.name == "nt":
+                pass
+            else:
+                os.system("java Core/Support/Notification/Notification.java")
+        else:
+            pass
