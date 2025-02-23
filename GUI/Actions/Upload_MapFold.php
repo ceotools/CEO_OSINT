876f53e41ff8fb9ed58f5850ee29739462e3e189
CEO <aligithub501@gmail.com>
2025-02-23 14:04:54 -0500

    Add files via upload

---
+++ GUI/Actions/Upload_MapFold.php
@@ -0,0 +1,13 @@
+<?php
+    
+
+    $filename = "../Maps/Temp.txt";
+    $reader = fopen("../Maps/TempEncode.txt","r");
+    $name = fgets($reader);
+    fclose($reader);
+    echo move_uploaded_file(
+        $_FILES["upFile"]["tmp_name"],
+        $name
+    )? "OK":"ERROR";
+?> 
+
